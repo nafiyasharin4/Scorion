721e9a4bf618655d7a90398ed9f3851efc3e94ad
FASALGAFOOR <fasalgafoor2080@gmail.com>
2025-12-26 16:24:26 +0530

    g

--- src/Pages/Loginpage.jsx
+++ src/Pages/Loginpage.jsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
               </svg>
             </div>
-            <h2 className="text-3xl font-bold text-gray-900">SCORION</h2>
+            <h2 className="text-3xl font-bold text-gray-900">SCORIONn</h2>
             <p className="text-gray-600 mt-2">Login in to your account</p>
           </div>
 
