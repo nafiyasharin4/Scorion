e62784671abf0b7fe0ec3b3ec5f86d53eb5840d5
FASALGAFOOR <fasalgafoor2080@gmail.com>
2026-03-02 15:18:57 +0530

    fixing issues on renderlink

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL = 'http://localhost:5000';
-// export const API_BASE_URL = 'https://scorion-backend.onrender.com';
+// export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://scorion-backend.onrender.com'; 
+// added render backend link
